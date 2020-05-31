77daa3138b61c44f3b5f0078e0b2cc7d071235db
Everton <everton.rodrigues1104@gmail.com>
2020-05-30 23:56:30 -0300

    Arrumado leitura de dados!

--- CardSys/src/Main.java
+++ CardSys/src/Main.java
@@ -4,20 +4,34 @@ import com.fazecast.jSerialComm.SerialPort;
 
 public class Main {
 
-	public static void main(String[] args) {
-		SerialPort comPort = SerialPort.getCommPorts()[0];
-		comPort.openPort();
-		comPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0);
-		InputStream in = comPort.getInputStream();
-		try
-		{
-			while(true){//manter o projeto aberto.
-				for(int i=0; i <10; i++) {
-					//chamar classes a pertir desta.
-				}
-				in.close();
+	public static void main(String[] args){
+		
+		
+		new Thread() {
+			@Override
+			public void run(){
+				try {
+					SerialPort comPort = SerialPort.getCommPort("COM5");
+					comPort.openPort();
+					comPort.setComPortTimeouts(SerialPort.TIMEOUT_READ_SEMI_BLOCKING, 0, 0);
+					InputStream in = comPort.getInputStream();
+					boolean loop = true;
+					while (loop) {
+						String res = "";
+						for (int i = 0; i < 10; i++) {
+							res += (char) in.read();
+							Thread.sleep(10);
+						}
+						in.close();
+						System.out.println(res);
+						res = "";
+					}
+					comPort.closePort();
+				} catch (Exception e) {}
 			}
-		} catch (Exception e) { e.printStackTrace(); }
-		comPort.closePort();
+		}.start();
+		
+		
+		
 	}
 }
