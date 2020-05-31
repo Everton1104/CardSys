76d56dd023bba1b7766fdbd376f85fe81999d976
Everton <everton.rodrigues1104@gmail.com>
2020-05-31 04:31:25 -0300

    fix ard.ler em string id.

--- CardSys/src/MostraCliente.java
+++ CardSys/src/MostraCliente.java
@@ -8,6 +8,9 @@ import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.Color;
 import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class MostraCliente extends JFrame {
 
