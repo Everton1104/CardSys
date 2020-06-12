2a195394c2d4357f211eaa19b145f651a1a2042b
Everton <everton.rodrigues1104@gmail.com>
2020-06-11 22:57:12 -0300

    okok

--- CardSys/src/br/com/CardSys/modelo/testaCliente.java
+++ CardSys/src/br/com/CardSys/modelo/testaCliente.java
@@ -1,6 +1,5 @@
 package br.com.CardSys.modelo;
 
-import javax.net.ssl.ManagerFactoryParameters;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
