95c3d5616ec683f8394cfc54f4320736615a2543
Everton <everton.rodrigues1104@gmail.com>
2020-06-14 15:21:09 -0300

    scroll list pronta

--- CardSys/src/br/com/CardSys/Main.java
+++ CardSys/src/br/com/CardSys/Main.java
@@ -8,8 +8,6 @@ public class Main {
 		tela.setVisible(true);
 		tela.cliente(ard.ler());
 		tela.setVisible(true);
-		
-
 	}
 
 }
--- CardSys/src/br/com/CardSys/MontaTela.java
+++ CardSys/src/br/com/CardSys/MontaTela.java
@@ -2,19 +2,20 @@ package br.com.CardSys;
 
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.ScrollPane;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+import javax.swing.AbstractListModel;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
-
-import jdk.jfr.Event;
-
-import javax.swing.JButton;
+import javax.swing.border.LineBorder;
 
 public class MontaTela extends JFrame {
 	private static final long serialVersionUID = 1L;
@@ -24,7 +25,7 @@ public class MontaTela extends JFrame {
 	
 	public MontaTela() throws Exception{
 		inicio();
-		//cliente("teste");
+		//cliente("92518414");
 	}
 	
 
@@ -57,7 +58,7 @@ public class MontaTela extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(200, 0, 1024, 768);
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setBorder(new LineBorder(new Color(0, 0, 0)));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
@@ -79,28 +80,49 @@ public class MontaTela extends JFrame {
 			JLabel lblNewLabel_1_2 = new JLabel("Cliente:");
 			lblNewLabel_1_2.setHorizontalAlignment(SwingConstants.CENTER);
 			lblNewLabel_1_2.setFont(new Font("Tahoma", Font.PLAIN, 50));
-			lblNewLabel_1_2.setBounds(50, 180, 389, 86);
+			lblNewLabel_1_2.setBounds(60, 129, 389, 86);
 			contentPane.add(lblNewLabel_1_2);
 		
 			JLabel lblNewLabel_1_1_1 = new JLabel(cliente.get(0));
 			lblNewLabel_1_1_1.setHorizontalAlignment(SwingConstants.CENTER);
 			lblNewLabel_1_1_1.setForeground(Color.ORANGE);
 			lblNewLabel_1_1_1.setFont(new Font("Tahoma", Font.PLAIN, 50));
-			lblNewLabel_1_1_1.setBounds(466, 180, 497, 86);
+			lblNewLabel_1_1_1.setBounds(466, 129, 497, 86);
 			contentPane.add(lblNewLabel_1_1_1);
 			
 			JLabel lblNewLabel_1_2_1 = new JLabel("Telefone:");
 			lblNewLabel_1_2_1.setHorizontalAlignment(SwingConstants.CENTER);
 			lblNewLabel_1_2_1.setFont(new Font("Tahoma", Font.PLAIN, 50));
-			lblNewLabel_1_2_1.setBounds(50, 326, 389, 86);
+			lblNewLabel_1_2_1.setBounds(50, 225, 389, 86);
 			contentPane.add(lblNewLabel_1_2_1);
 			
 			JLabel lblNewLabel_1_1_1_1 = new JLabel(cliente.get(1));
 			lblNewLabel_1_1_1_1.setHorizontalAlignment(SwingConstants.CENTER);
 			lblNewLabel_1_1_1_1.setForeground(Color.ORANGE);
 			lblNewLabel_1_1_1_1.setFont(new Font("Tahoma", Font.PLAIN, 50));
-			lblNewLabel_1_1_1_1.setBounds(466, 326, 497, 86);
+			lblNewLabel_1_1_1_1.setBounds(466, 225, 497, 86);
 			contentPane.add(lblNewLabel_1_1_1_1);
+			
+			
+			ScrollPane scrollPane = new ScrollPane();
+			scrollPane.setBounds(10, 317, 979, 389);
+			JList<String> list = new JList<>(new AbstractListModel<String>() {
+				private static final long serialVersionUID = 1L;
+				String[] values = new String[] {
+						"teste", 
+						"teste"};
+				public int getSize() {
+					return values.length;
+				}
+				public String getElementAt(int index) {
+					return values[index];
+				}
+			});
+			list.setFont(new Font("Tahoma", Font.PLAIN, 30));
+			list.setBounds(10, 321, 979, 385);
+			scrollPane.add(list);
+			contentPane.add(scrollPane);
+			
 		}else {
 			JLabel lblNewLabel_1_3 = new JLabel("Nome:");
 			lblNewLabel_1_3.setHorizontalAlignment(SwingConstants.CENTER);
